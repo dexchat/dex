46ec8e15bd34d12d56250723dc5f9ccd5c38f95c
Leonardo Essia <leonardo@essia.dev>
2025-12-31 19:11:09 -0300

    simplify layout code

--- internal/ui/layout/layout.go
+++ internal/ui/layout/layout.go
@@ -1,61 +1,7 @@
 package layout
 
-const (
-	InputBoxHeight     = 3 // top/bottom border + text input (1 line)
-	InputBoxMargin     = 1
-	InputBoxPaddingTop = 1
-
-	// Sidebars will occupy 10% of the terminal screen size
-	// TODO: if I increase the ratio of one sidebar, the other is not recalculated based on the difference, fix it
-	channelSidebarRatio = 0.1
-	userSidebarRatio    = 0.1
-
-	sidebarsPadding               = 2
-	appPadding                    = 2
-	verticalPadding               = 1
-	ChatViewportPaddingHorizontal = 2 // PaddingLeft(1) + PaddingRight(1)
-)
-
+// TODO: remove this package
 type Layout struct {
-	ScreenWidth  int
-	ScreenHeight int
-
-	AppHeight     int
-	ChatWidth     int
-	SiderbarWidth int
-}
-
-// GenerateLayout computes the full layout based on the terminal screen size
-func GenerateLayout(terminalWidth, terminalHeight int) Layout {
-	appAvailableHeight := terminalHeight - verticalPadding
-	if appAvailableHeight < 0 {
-		appAvailableHeight = 0 // terminal too small, TODO: prevent app to open in these checks
-	}
-
-	rawChannelsWidth := int(float64(terminalWidth) * channelSidebarRatio)
-	rawUsersWidth := int(float64(terminalWidth) * userSidebarRatio)
-
-	adjustedSidebarWidth := rawChannelsWidth - sidebarsPadding
-	adjustedChatWidth := terminalWidth - rawChannelsWidth - rawUsersWidth - appPadding
-
-	if adjustedSidebarWidth < 0 {
-		adjustedSidebarWidth = 0
-	}
-	if adjustedChatWidth < 0 {
-		adjustedChatWidth = 0
-	}
-
-	totalHorizontalPadding := (sidebarsPadding * 2) + appPadding
-
-	// chat left border (1) + chat right border (1) +
-	// right sidebar left border (1) + left sidebar right border (1)
-	horizontalBorderSum := 4
-
-	return Layout{
-		ScreenWidth:   terminalWidth - totalHorizontalPadding,
-		ScreenHeight:  terminalHeight,
-		AppHeight:     appAvailableHeight - verticalPadding,
-		ChatWidth:     adjustedChatWidth - horizontalBorderSum,
-		SiderbarWidth: adjustedSidebarWidth,
-	}
+	Width  int
+	Height int
 }
