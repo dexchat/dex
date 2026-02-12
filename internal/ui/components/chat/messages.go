5e5cf458ec175990171d745514d1b1413ccb2ba8
Leonardo Essia <leonardo@essia.dev>
2026-02-12 09:04:54 -0300

    fix: render IRC formatted messages also in server buffers

--- internal/ui/components/chat/messages.go
+++ internal/ui/components/chat/messages.go
@@ -23,28 +23,29 @@ func (m *Model) renderMessage(msg Message, width int) string {
 
 	styledTime := timeStyle.Render(msg.Timestamp.Format("15:04"))
 
+	styledText := renderIRCFormattedMessage(msg.Text, baseStyle)
+
 	switch {
 	case msg.Username == "-->" || msg.Username == "<--":
 		eventStyle := baseStyle.Foreground(m.theme.Colors.Chat.UserEvents)
-		return baseStyle.Width(width).Render(styledTime + eventStyle.Render(" "+msg.Username+" "+msg.Text))
+		return baseStyle.Width(width).Render(styledTime + eventStyle.Render(" "+msg.Username+" ") + styledText)
 
 	case msg.Type == irc.MessageTypeConnected:
 		textStyle := baseStyle.Foreground(m.theme.Colors.Chat.Connected)
-		return baseStyle.Width(width).Render(styledTime + textStyle.Render(" "+msg.Username+" "+msg.Text))
+		return baseStyle.Width(width).Render(styledTime + textStyle.Render(" "+msg.Username+" ") + styledText)
 
 	case msg.Type == irc.MessageTypeDisconnected:
 		textStyle := baseStyle.Foreground(m.theme.Colors.Chat.Disconnected)
-		return baseStyle.Width(width).Render(styledTime + textStyle.Render(" "+msg.Username+" "+msg.Text))
+		return baseStyle.Width(width).Render(styledTime + textStyle.Render(" "+msg.Username+" ") + styledText)
 
 	case msg.Type == irc.MessageTypeServer:
 		serverStyle := baseStyle.Foreground(m.theme.Colors.Chat.ServerMessage)
-		return baseStyle.Width(width).Render(styledTime + serverStyle.Render(" "+msg.Username+" "+msg.Text))
+		return baseStyle.Width(width).Render(styledTime + serverStyle.Render(" "+msg.Username+" ") + styledText)
 
 	default:
 		nickStyle := baseStyle.
 			Foreground(m.usernameColors.GetColor(msg.Username))
 		styledNick := nickStyle.Render(" " + msg.Username + " ")
-		styledText := renderIRCFormattedMessage(msg.Text, baseStyle)
 		return baseStyle.Width(width).Render(styledTime + styledNick + styledText)
 	}
 }
